c2752f0c47e0cd0341b417807d2de8229532a159
ddeejjvviidd <david.salek@tul.cz>
2025-02-20 19:36:44 +0100

    Initial

---
+++ build/CMakeFiles/pg2_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pg2_project.dir/src/main.cpp.o"
+  "CMakeFiles/pg2_project.dir/src/main.cpp.o.d"
+  "pg2_project"
+  "pg2_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pg2_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
