c9bd9e173b53bb990769c6207eded6683696702f
Lilian Luca <lilianluca2118@gmail.com>
2025-03-05 20:49:46 +0100

    cv03

--- src/app.cpp
+++ src/app.cpp
@@ -16,10 +16,10 @@
 #include <opencv2\opencv.hpp>
 
 // OpenGL Extension Wrangler: allow all multiplatform GL functions
-#include <GL/glew.h> 
-// WGLEW = Windows GL Extension Wrangler (change for different platform) 
+#include <GL/glew.h>
+// WGLEW = Windows GL Extension Wrangler (change for different platform)
 // platform specific functions (in this case Windows)
-#include <GL/wglew.h> 
+#include <GL/wglew.h>
 
 // GLFW toolkit
 // Uses GL calls to open GL context, i.e. GLEW __MUST__ be first.
@@ -33,6 +33,7 @@
 
 #include "app.hpp"
 #include "gl_err_callback.h"
+#include "assets.hpp"
 
 using json = nlohmann::json; // Alias for convenience
 
@@ -41,12 +42,12 @@ App::App()
 	// default constructor
 	// nothing to do here (so far...)
 	std::cout << "Constructed...\n";
-
 }
 
 bool App::init()
 {
-	try {
+	try
+	{
 		// initialization code
 		//...
 		// init glfw
@@ -57,32 +58,40 @@ bool App::init()
 
 		// Load settings from app_settings.json
 		std::ifstream settingsFile("app_settings.json");
-		if (settingsFile.is_open()) {
-			try {
+		if (settingsFile.is_open())
+		{
+			try
+			{
 				json settings = json::parse(settingsFile);
 
 				// Set appname (with fallback)
-				if (settings.contains("appname") && settings["appname"].is_string()) {
+				if (settings.contains("appname") && settings["appname"].is_string())
+				{
 					appname = settings["appname"].get<std::string>();
 				}
 
 				// Set default_resolution (with fallback)
-				if (settings.contains("default_resolution") && settings["default_resolution"].is_object()) {
-					if (settings["default_resolution"].contains("x") && settings["default_resolution"]["x"].is_number_integer()) {
+				if (settings.contains("default_resolution") && settings["default_resolution"].is_object())
+				{
+					if (settings["default_resolution"].contains("x") && settings["default_resolution"]["x"].is_number_integer())
+					{
 						resX = settings["default_resolution"]["x"].get<int>();
 					}
-					if (settings["default_resolution"].contains("y") && settings["default_resolution"]["y"].is_number_integer()) {
+					if (settings["default_resolution"].contains("y") && settings["default_resolution"]["y"].is_number_integer())
+					{
 						resY = settings["default_resolution"]["y"].get<int>();
 					}
 				}
 			}
-			catch (const json::exception& e) {
+			catch (const json::exception &e)
+			{
 				std::cerr << "JSON parsing error: " << e.what() << '\n';
 				// Continue with defaults
 			}
 			settingsFile.close();
 		}
-		else {
+		else
+		{
 			std::cerr << "Could not open app_settings.json, using defaults\n";
 		}
 
@@ -96,12 +105,13 @@ bool App::init()
 		window = glfwCreateWindow(resX, resY, appname.c_str(), NULL, NULL);
 		glfwMakeContextCurrent(window);
 		glfwSetWindowUserPointer(window, this);
-		glfwSetScrollCallback(window, [](GLFWwindow* w, double x, double y) {
-			static_cast<App*>(glfwGetWindowUserPointer(w))->scroll_callback(w, x, y);
-		});
-		glfwSetKeyCallback(window, [](GLFWwindow* w, int k, int s, int a, int m) {
-			static_cast<App*>(glfwGetWindowUserPointer(w))->key_callback(w, k, s, a, m);
-		});
+		glfwSetScrollCallback(window, [](GLFWwindow *w, double x, double y)
+							  { static_cast<App *>(glfwGetWindowUserPointer(w))->scroll_callback(w, x, y); });
+		glfwSetKeyCallback(window, [](GLFWwindow *w, int k, int s, int a, int m)
+						   { static_cast<App *>(glfwGetWindowUserPointer(w))->key_callback(w, k, s, a, m); });
+		// Add mouse button callback with lambda
+		glfwSetMouseButtonCallback(window, [](GLFWwindow *w, int button, int action, int mods)
+								   { static_cast<App *>(glfwGetWindowUserPointer(w))->mouse_button_callback(w, button, action, mods); });
 
 		// init glew
 		// http://glew.sourceforge.net/basic.html
@@ -110,9 +120,9 @@ bool App::init()
 		// if (not_success)
 		//  throw std::runtime_error("something went bad");
 
-		// HOWTO get string safely ( glGetString() may return nullptr ) 
+		// HOWTO get string safely ( glGetString() may return nullptr )
 		// Get OpenGL version
-		const char* openGLVersion = (const char*)glGetString(GL_VERSION);
+		const char *openGLVersion = (const char *)glGetString(GL_VERSION);
 		if (openGLVersion == nullptr)
 			std::cout << "<Unknown>\n";
 		else
@@ -123,25 +133,32 @@ bool App::init()
 		GLint myint;
 		glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &myint);
 
-		if (myint & GL_CONTEXT_CORE_PROFILE_BIT) {
+		if (myint & GL_CONTEXT_CORE_PROFILE_BIT)
+		{
 			std::cout << "We are using CORE profile\n";
 		}
-		else {
-			if (myint & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT) {
+		else
+		{
+			if (myint & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT)
+			{
 				std::cout << "We are using COMPATIBILITY profile\n";
 			}
-			else {
+			else
+			{
 				throw std::runtime_error("What??");
 			}
 		}
+
+		if (!GLEW_ARB_direct_state_access)
+			throw std::runtime_error("No DSA :-(");
 
 		if (GLEW_ARB_debug_output)
 		{
 			glDebugMessageCallback(MessageCallback, 0);
 			glEnable(GL_DEBUG_OUTPUT);
 
-			//default is asynchronous debug output, use this to simulate glGetError() functionality
-			//glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+			// default is asynchronous debug output, use this to simulate glGetError() functionality
+			// glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 
 			std::cout << "GL_DEBUG enabled." << std::endl;
 		}
@@ -150,8 +167,11 @@ bool App::init()
 
 		// Set initial VSync state
 		glfwSwapInterval(vsyncEnabled ? 1 : 0);
+
+		init_assets();
 	}
-	catch (std::exception const& e) {
+	catch (std::exception const &e)
+	{
 		std::cerr << "Init failed : " << e.what() << std::endl;
 		throw;
 	}
@@ -160,16 +180,92 @@ bool App::init()
 	return true;
 }
 
+void App::init_assets(void)
+{
+	//
+	// Initialize pipeline: compile, link and use shaders
+	//
+
+	// SHADERS - define & compile & link
+	const char *vertex_shader =
+		"#version 460 core\n"
+		"in vec3 attribute_Position;"
+		"void main() {"
+		"  gl_Position = vec4(attribute_Position, 1.0);"
+		"}";
+
+	const char *fragment_shader =
+		"#version 460 core\n"
+		"uniform vec4 uniform_Color;"
+		"out vec4 FragColor;"
+		"void main() {"
+		"  FragColor = uniform_Color;"
+		"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	shader_prog_ID = glCreateProgram();
+	glAttachShader(shader_prog_ID, fs);
+	glAttachShader(shader_prog_ID, vs);
+	glLinkProgram(shader_prog_ID);
+
+	// now we can delete shader parts (they can be reused, if you have more shaders)
+	// the final shader program already linked and stored separately
+	glDetachShader(shader_prog_ID, fs);
+	glDetachShader(shader_prog_ID, vs);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	//
+	// Create and load data into GPU using OpenGL DSA (Direct State Access)
+	//
+
+	// Create VAO + data description (just envelope, or container...)
+	glCreateVertexArrays(1, &VAO_ID);
+
+	GLint position_attrib_location = glGetAttribLocation(shader_prog_ID, "attribute_Position");
+
+	glEnableVertexArrayAttrib(VAO_ID, position_attrib_location);
+	glVertexArrayAttribFormat(VAO_ID, position_attrib_location, 3, GL_FLOAT, GL_FALSE, offsetof(vertex, position));
+	glVertexArrayAttribBinding(VAO_ID, position_attrib_location, 0); // (GLuint vaobj, GLuint attribindex, GLuint bindingindex)
+
+	// Create and fill data
+	glCreateBuffers(1, &VBO_ID);
+	glNamedBufferData(VBO_ID, triangle_vertices.size() * sizeof(vertex), triangle_vertices.data(), GL_STATIC_DRAW);
+
+	// Connect together
+	glVertexArrayVertexBuffer(VAO_ID, 0, VBO_ID, 0, sizeof(vertex)); // (GLuint vaobj, GLuint bindingindex, GLuint buffer, GLintptr offset, GLsizei stride)
+}
+
 int App::run(void)
 {
-	if (!window) return -1;
+	if (!window)
+		return -1;
 
 	double lastTime = glfwGetTime(); // Time of last FPS update
-	int frameCount = 0; // Number of frames since last update
+	int frameCount = 0;				 // Number of frames since last update
 
-	try {
+	try
+	{
 		// app code
 		//...
+
+		// Activate shader program. There is only one program, so activation can be out of the loop.
+		// In more realistic scenarios, you will activate different shaders for different 3D objects.
+		glUseProgram(shader_prog_ID);
+
+		// Get uniform location in GPU program. This will not change, so it can be moved out of the game loop.
+		GLint uniform_color_location = glGetUniformLocation(shader_prog_ID, "uniform_Color");
+		if (uniform_color_location == -1)
+		{
+			std::cerr << "Uniform location is not found in active shader program. Did you forget to activate it?\n";
+		}
 
 		while (!glfwWindowShouldClose(window))
 		{
@@ -178,10 +274,11 @@ int App::run(void)
 			frameCount++;
 
 			// Update FPS every second
-			if (currentTime - lastTime >= 1.0) {
+			if (currentTime - lastTime >= 1.0)
+			{
 				double fps = frameCount / (currentTime - lastTime);
 				std::string title = "FPS: " + std::to_string(static_cast<int>(fps + 0.5)) +
-					" | VSync: " + (vsyncEnabled ? "On" : "Off");
+									" | VSync: " + (vsyncEnabled ? "On" : "Off");
 				glfwSetWindowTitle(window, title.c_str());
 
 				frameCount = 0;
@@ -191,14 +288,24 @@ int App::run(void)
 			// Clear OpenGL canvas, both color buffer and Z-buffer
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			// Swap front and back buffers
-			glfwSwapBuffers(window);
+			// set uniform parameter for shader
+			// (try to change the color in key callback)
+			glUniform4f(uniform_color_location, r, g, b, a);
+
+			// bind 3d object data
+			glBindVertexArray(VAO_ID);
+
+			// draw all VAO data
+			glDrawArrays(GL_TRIANGLES, 0, triangle_vertices.size());
 
 			// Poll for and process events
 			glfwPollEvents();
+			// Swap front and back buffers
+			glfwSwapBuffers(window);
 		}
 	}
-	catch (std::exception const& e) {
+	catch (std::exception const &e)
+	{
 		std::cerr << "App failed : " << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
@@ -207,25 +314,62 @@ int App::run(void)
 	return EXIT_SUCCESS;
 }
 
-void App::error_callback(int error, const char* description) {
+void App::error_callback(int error, const char *description)
+{
 	std::cerr << "GLFW Error " << error << ": " << description << '\n';
 }
 
-
-void App::scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+void App::scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
 	std::cout << "Scroll: " << xoffset << ", " << yoffset << '\n';
 }
 
-void App::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	if ((action == GLFW_PRESS) || (action == GLFW_REPEAT)) {
-		switch (key) {
+void App::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	if ((action == GLFW_PRESS) || (action == GLFW_REPEAT))
+	{
+		switch (key)
+		{
 		case GLFW_KEY_ESCAPE:
 			glfwSetWindowShouldClose(window, GLFW_TRUE);
 			break;
-		case GLFW_KEY_F12: // Changed from GLFW_KEY_V to GLFW_KEY_F12
-			vsyncEnabled = !vsyncEnabled; // Toggle VSync state
+		case GLFW_KEY_F12:							// Changed from GLFW_KEY_V to GLFW_KEY_F12
+			vsyncEnabled = !vsyncEnabled;			// Toggle VSync state
 			glfwSwapInterval(vsyncEnabled ? 1 : 0); // Apply VSync setting
 			// Title will update in the next FPS cycle
+			break;
+		case GLFW_KEY_R:
+			if (r == 0.0f)
+			{
+				r = 1.0f;
+			}
+			else if (r == 1.0f)
+			{
+				r = 0.0f;
+			}
+			std::cout << "r = " << r << std::endl;
+			break;
+		case GLFW_KEY_G:
+			if (g == 0.0f)
+			{
+				g = 1.0f;
+			}
+			else if (g == 1.0f)
+			{
+				g = 0.0f;
+			}
+			std::cout << "g = " << g << std::endl;
+			break;
+		case GLFW_KEY_B:
+			if (b == 0.0f)
+			{
+				b = 1.0f;
+			}
+			else if (b == 1.0f)
+			{
+				b = 0.0f;
+			}
+			std::cout << "b = " << b << std::endl;
 			break;
 		default:
 			break;
@@ -233,6 +377,31 @@ void App::key_callback(GLFWwindow* window, int key, int scancode, int action, in
 	}
 }
 
+void App::mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		if (r == 1.0f)
+		{
+			r = 0.0f;
+			g = 1.0f;
+			b = 0.0f;
+		}
+		else if (g == 1.0f)
+		{
+			r = 0.0f;
+			g = 0.0f;
+			b = 1.0f;
+		}
+		else if (b == 1.0f)
+		{
+			r = 1.0f;
+			g = 0.0f;
+			b = 0.0f;
+		}
+		std::cout << "Mouse left click, r = " << r << ", g = " << g << ", b = " << b << std::endl;
+	}
+}
 
 App::~App()
 {
@@ -240,6 +409,11 @@ App::~App()
 	if (window)
 		glfwDestroyWindow(window);
 	glfwTerminate();
+
+	// cleanup GL data
+	glDeleteProgram(shader_prog_ID);
+	glDeleteBuffers(1, &VBO_ID);
+	glDeleteVertexArrays(1, &VAO_ID);
 
 	exit(EXIT_SUCCESS);
 	cv::destroyAllWindows();
--- src/app.hpp
+++ src/app.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-#include <GLFW/glfw3.h>
+#include <GL/glew.h>    // Add this to ensure GLEW comes first
+#include "assets.hpp"   // Already includes glew.h, but we make it explicit
+#include <GLFW/glfw3.h> // GLFW comes after GLEW
 #include <string>
+#include <vector>
 
 class App
 {
@@ -9,10 +12,24 @@ public:
     App();
 
     bool init(void);
+    void init_assets(void);
     int run(void);
     static void error_callback(int error, const char *description);
     void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
     void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+    void mouse_button_callback(GLFWwindow *window, int button, int action, int mods); // New mouse callback
+
+    GLuint shader_prog_ID{0};
+    GLuint VBO_ID{0};
+    GLuint VAO_ID{0};
+
+    GLfloat r{1.0f}, g{0.0f}, b{0.0f}, a{1.0f};
+
+    std::vector<vertex> triangle_vertices =
+        {
+            {{0.0f, 0.5f, 0.0f}},
+            {{0.5f, -0.5f, 0.0f}},
+            {{-0.5f, -0.5f, 0.0f}}};
 
     ~App();
 
